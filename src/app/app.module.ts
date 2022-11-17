f1a78b7d7379a953505f148b84aedf91d0e7c52b
Maxi Olea <molea@mobydigital.com>
2022-11-17 13:32:29 -0300

    firebase production fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,12 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     IonicModule.forRoot(), 
     AppRoutingModule,
     SharedModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+    // ServiceWorkerModule.register('ngsw-worker.js', {
+    //   enabled: !isDevMode(),
+    //   // Register the ServiceWorker as soon as the application is stable
+    //   // or after 30 seconds (whichever comes first).
+    //   registrationStrategy: 'registerWhenStable:30000'
+    // })
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
